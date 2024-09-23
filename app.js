7dccb00582ca4afab371954d4e54819986127cd9
Habtemaraim Bereket <habteshbaki2014@gmail.com>
2024-09-23 03:50:16 -0700

    I have made some modification

--- app.js
+++ app.js
@@ -17,9 +17,7 @@ app.use(cors({
 
 app.use(express.json());
 
-// app.use('/' ,(req,res) {
-//    res.send('hello')
-// })
+
 app.use('/api/users', userRoute)
 app.use('/api/products', productRoute)
 app.use('/api/orders', orderRoute)
